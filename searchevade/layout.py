0e2e79cc6dd3b18d88d9bd90ebd564f668c6c0f3
Leon Yao <leonxyao@gmail.com>
2013-11-21 16:50:16 -0800

    init

---
+++ searchevade/layout.py
@@ -0,0 +1,189 @@
+from util import manhattanDistance
+from game import Grid
+import os
+import random
+
+from collections import defaultdict
+
+VISIBILITY_MATRIX_CACHE = {}
+
+class Layout:
+  """
+  A Layout manages the static information about the game board.
+  """
+  
+  def __init__(self, layoutText, roomText):
+    self.width = len(layoutText[0])
+    self.height= len(layoutText)
+    self.walls = Grid(self.width, self.height, False)
+    self.doors = Grid(self.width, self.height, False)
+    self.food = Grid(self.width, self.height, False)
+    self.capsules = []
+    self.agentPositions = []
+    self.numGhosts = 0
+    self.layoutText = layoutText
+    self.roomText = roomText
+
+    self.room = Grid(self.width, self.height, '%')
+    self.rooms_mapping = defaultdict(list)
+
+    self.processLayoutText(layoutText,roomText)
+
+    print self.rooms_mapping
+    # self.initializeVisibilityMatrix()
+    
+  def getNumGhosts(self):
+    return self.numGhosts
+    
+  def initializeVisibilityMatrix(self):
+    global VISIBILITY_MATRIX_CACHE
+    if reduce(str.__add__, self.layoutText) not in VISIBILITY_MATRIX_CACHE:
+      from game import Directions
+      vecs = [(-0.5,0), (0.5,0),(0,-0.5),(0,0.5)]
+      dirs = [Directions.NORTH, Directions.SOUTH, Directions.WEST, Directions.EAST]
+      vis = Grid(self.width, self.height, {Directions.NORTH:set(), Directions.SOUTH:set(), Directions.EAST:set(), Directions.WEST:set(), Directions.STOP:set()})
+      for x in range(self.width):
+        for y in range(self.height):
+          if self.walls[x][y] == False:
+            for vec, direction in zip(vecs, dirs):
+              dx, dy = vec
+              nextx, nexty = x + dx, y + dy
+              while (nextx + nexty) != int(nextx) + int(nexty) or not self.walls[int(nextx)][int(nexty)] :
+                vis[x][y][direction].add((nextx, nexty))
+                nextx, nexty = x + dx, y + dy
+      self.visibility = vis      
+      VISIBILITY_MATRIX_CACHE[reduce(str.__add__, self.layoutText)] = vis
+    else:
+      self.visibility = VISIBILITY_MATRIX_CACHE[reduce(str.__add__, self.layoutText)]
+      
+  def isWall(self, pos):
+    x, col = pos
+    return self.walls[x][col]
+
+  def isDoor(self,pos):
+    x, col = pos
+    return self.walls[x][col]
+  
+  def getRandomLegalPosition(self):
+    x = random.choice(range(self.width))
+    y = random.choice(range(self.height))
+    while self.isWall( (x, y) ):
+      x = random.choice(range(self.width))
+      y = random.choice(range(self.height))
+    return (x,y)
+
+  def getRandomCorner(self):
+    poses = [(1,1), (1, self.height - 2), (self.width - 2, 1), (self.width - 2, self.height - 2)]
+    return random.choice(poses)
+
+  def getFurthestCorner(self, pacPos):
+    poses = [(1,1), (1, self.height - 2), (self.width - 2, 1), (self.width - 2, self.height - 2)]
+    dist, pos = max([(manhattanDistance(p, pacPos), p) for p in poses])
+    return pos
+  
+  def isVisibleFrom(self, ghostPos, pacPos, pacDirection):
+    row, col = [int(x) for x in pacPos]
+    return ghostPos in self.visibility[row][col][pacDirection]
+  
+  def __str__(self):
+    return "\n".join(self.layoutText)
+    
+  def deepCopy(self):
+    return Layout(self.layoutText[:],self.roomText[:])
+    
+  def processLayoutText(self, layoutText,roomText):
+    """
+    Coordinates are flipped from the input format to the (x,y) convention here
+    
+    The shape of the maze.  Each character  
+    represents a different type of object.   
+     % - Wall                               
+     . - Food
+     o - Capsule
+     G - Ghost
+     P - Pacman
+     | - Door
+    Other characters are ignored.
+    """
+    # x = 1
+    # assert(x==2)
+    maxY = self.height - 1
+    for y in range(self.height):       
+      for x in range(self.width):
+        layoutChar = layoutText[maxY - y][x]  
+        roomChar = roomText[maxY - y][x]
+        self.processLayoutChar(x, y, layoutChar)
+        self.processRoomChar(x, y, roomChar)
+    self.agentPositions.sort()
+    self.agentPositions = [ ( i == 0, pos) for i, pos in self.agentPositions]
+    for y in range(self.height):
+      for x in range(self.width):
+        room = self.room[x][y]
+        if room != '%' and room != '|': 
+          self.rooms_mapping[room].append((x,y))
+        elif room == '|':
+          self.rooms_mapping['door'].append((x,y))
+        elif room == '%':
+          self.rooms_mapping['wall'].append((x,y))
+  
+  def processLayoutChar(self, x, y, layoutChar):
+    if layoutChar == '%':      
+      self.walls[x][y] = True
+    elif layoutChar == '|':
+      self.doors[x][y] = True
+    elif layoutChar == '.':
+      self.food[x][y] = True 
+    elif layoutChar == 'o':    
+      self.capsules.append((x, y))   
+    elif layoutChar == 'P':    
+      self.agentPositions.append( (0, (x, y) ) )
+    elif layoutChar in ['G']:    
+      self.agentPositions.append( (1, (x, y) ) )
+      self.numGhosts += 1
+    elif layoutChar in  ['1', '2', '3', '4']:
+      self.agentPositions.append( (int(layoutChar), (x,y)))
+      self.numGhosts += 1 
+
+  def processRoomChar(self, x, y, roomChar):
+    if roomChar == '%' or roomChar == '|':
+      self.room[x][y] = roomChar
+    elif roomChar.isalpha() and roomChar.islower:
+      self.room[x][y] = roomChar
+    elif roomChar == 'H':
+      self.room[x][y] = 'H'
+
+def getLayout(name, back = 2):
+  if name.endswith('.lay'):
+    name = name[:len(name)-4]
+    layout = tryToLoad('layouts/' + name)
+    if layout == None: layout = tryToLoad(name)
+  else:
+    layout = tryToLoad('layouts/' + name)
+    if layout == None: layout = tryToLoad(name)
+  if layout == None and back >= 0:
+    curdir = os.path.abspath('.')
+    os.chdir('..')
+    layout = getLayout(name, back -1)
+    os.chdir(curdir)
+  return layout
+
+# def getRoom(name, back = 2):
+#   if name.endswith('.lay'):
+#     layout = tryToLoad('layouts/' + name + 'room')
+#     if layout == None: layout = tryToLoad(name + 'room')
+#   else:
+#     layout = tryToLoad('layouts/' + name + 'room' + '.lay')
+#     if layout == None: layout = tryToLoad(name + '.lay')
+#   if layout == None and back >= 0:
+#     curdir = os.path.abspath('.')
+#     os.chdir('..')
+#     layout = getRoom(name, back -1)
+#     os.chdir(curdir)
+#   return layout
+
+def tryToLoad(fullname):
+  if(not os.path.exists(fullname + '.lay')): return None
+  f = open(fullname + '.lay')
+  f_room = open(fullname + 'room.lay')
+  try: return Layout([line.strip() for line in f],[line.strip() for line in f_room] )
+  finally: f.close()
---
+++ searchevade/submission.py
@@ -0,0 +1,388 @@
+from util import manhattanDistance
+from game import Directions
+import random, util, sys
+
+from game import Agent
+
+class ReflexAgent(Agent):
+  """
+    A reflex agent chooses an action at each choice point by examining
+    its alternatives via a state evaluation function.
+
+    The code below is provided as a guide.  You are welcome to change
+    it in any way you see fit, so long as you don't touch our method
+    headers.
+  """
+  def __init__(self):
+    self.lastPositions = []
+    self.dc = None
+
+
+  def getAction(self, gameState):
+    """
+    getAction chooses among the best options according to the evaluation function.
+
+    getAction takes a GameState and returns some Directions.X for some X in the set {North, South, West, East, Stop}
+    ------------------------------------------------------------------------------
+    Description of GameState and helper functions:
+
+    A GameState specifies the full game state, including the food, capsules,
+    agent configurations and score changes. In this function, the |gameState| argument 
+    is an object of GameState class. Following are a few of the helper methods that you 
+    can use to query a GameState object to gather information about the present state 
+    of Pac-Man, the ghosts and the maze.
+    
+    gameState.getLegalActions(): 
+        Returns the legal actions for the agent specified. Returns Pac-Man's legal moves by default.
+
+    gameState.generateSuccessor(agentIndex, action): 
+        Returns the successor state after the specified agent takes the action. 
+        Pac-Man is always agent 0.
+
+    gameState.getPacmanState():
+        Returns an AgentState object for pacman (in game.py)
+        state.pos gives the current position
+        state.direction gives the travel vector
+
+    gameState.getGhostStates():
+        Returns list of AgentState objects for the ghosts
+
+    gameState.getNumAgents():
+        Returns the total number of agents in the game
+
+    
+    The GameState class is defined in pacman.py and you might want to look into that for 
+    other helper methods, though you don't need to.
+    """
+    # Collect legal moves and successor states
+    legalMoves = gameState.getLegalActions()
+
+    # Choose one of the best actions
+    scores = [self.evaluationFunction(gameState, action) for action in legalMoves]
+    bestScore = max(scores)
+    bestIndices = [index for index in range(len(scores)) if scores[index] == bestScore]
+    chosenIndex = random.choice(bestIndices) # Pick randomly among the best
+
+
+
+
+    return legalMoves[chosenIndex]
+
+  def evaluationFunction(self, currentGameState, action):
+    """
+    The evaluation function takes in the current and proposed successor
+    GameStates (pacman.py) and returns a number, where higher numbers are better.
+
+    The code below extracts some useful information from the state, like the
+    remaining food (oldFood) and Pacman position after moving (newPos).
+    newScaredTimes holds the number of moves that each ghost will remain
+    scared because of Pacman having eaten a power pellet.
+    """
+    # Useful information you can extract from a GameState (pacman.py)
+    successorGameState = currentGameState.generatePacmanSuccessor(action)
+    newPos = successorGameState.getPacmanPosition()
+    oldFood = currentGameState.getFood()
+    newGhostStates = successorGameState.getGhostStates()
+    newScaredTimes = [ghostState.scaredTimer for ghostState in newGhostStates]
+
+
+    return successorGameState.getScore()
+
+
+def scoreEvaluationFunction(currentGameState):
+  """
+    This default evaluation function just returns the score of the state.
+    The score is the same one displayed in the Pacman GUI.
+
+    This evaluation function is meant for use with adversarial search agents
+    (not reflex agents).
+  """
+  return currentGameState.getScore()
+
+class MultiAgentSearchAgent(Agent):
+  """
+    This class provides some common elements to all of your
+    multi-agent searchers.  Any methods defined here will be available
+    to the MinimaxPacmanAgent, AlphaBetaPacmanAgent & ExpectimaxPacmanAgent.
+
+    You *do not* need to make any changes here, but you can if you want to
+    add functionality to all your adversarial search agents.  Please do not
+    remove anything, however.
+
+    Note: this is an abstract class: one that should not be instantiated.  It's
+    only partially specified, and designed to be extended.  Agent (game.py)
+    is another abstract class.
+  """
+
+  def __init__(self, evalFn = 'scoreEvaluationFunction', depth = '2'):
+    self.index = 0 # Pacman is always agent index 0
+    self.evaluationFunction = util.lookup(evalFn, globals())
+    self.depth = int(depth)
+
+class MinimaxAgent(MultiAgentSearchAgent):
+  """
+    Your minimax agent (problem 1)
+  """
+  def getBestMinimaxValue(self,gameState,currDepth,agentNumber):
+    newDepth = currDepth
+    newAgentNumber = agentNumber
+    if agentNumber == gameState.getNumAgents():
+      newDepth+=1
+      newAgentNumber=0
+    if gameState.isWin() or gameState.isLose() or newDepth == self.depth: #depth start at 0
+      return (self.evaluationFunction(gameState),Directions.STOP)
+
+    elif newAgentNumber == 0:
+      return self.getMaxValue(gameState,newDepth,newAgentNumber) #just agent Number
+
+    elif newAgentNumber >= 1:
+      return self.getMinValue(gameState,newDepth,newAgentNumber)
+    else:
+      assert False
+
+  def getMaxValue(self,gameState,currDepth,agentNumber):
+    maxValue = -sys.maxint
+    bestAction = 0
+    assert agentNumber < gameState.getNumAgents()
+    assert agentNumber == 0
+    for action in gameState.getLegalActions(agentNumber):
+      if action == Directions.STOP:
+        continue
+      newGameState = gameState.generateSuccessor(agentNumber,action)
+      currMax = self.getBestMinimaxValue(newGameState,currDepth,agentNumber+1)[0]
+      if currMax > maxValue:
+        maxValue = currMax
+        bestAction = action
+    return (maxValue,bestAction)
+
+  def getMinValue(self,gameState,currDepth,agentNumber):
+    minValue = sys.maxint
+    bestAction = 0
+    assert agentNumber < gameState.getNumAgents()
+    assert agentNumber != 0
+    for action in gameState.getLegalActions(agentNumber):
+      if action == Directions.STOP:
+        continue
+      newGameState = gameState.generateSuccessor(agentNumber,action)
+      currMin = self.getBestMinimaxValue(newGameState,currDepth,agentNumber+1)[0]
+      if currMin < minValue:
+        minValue = currMin
+        bestAction = action
+    return (minValue,bestAction)
+
+
+
+  def getAction(self, gameState):
+    """
+      Returns the minimax action from the current gameState using self.depth
+      and self.evaluationFunction. Terminal states can be found by one of the following: 
+      pacman won, pacman lost or there are no legal moves. 
+
+      Here are some method calls that might be useful when implementing minimax.
+
+      gameState.getLegalActions(agentIndex):
+        Returns a list of legal actions for an agent
+        agentIndex=0 means Pacman, ghosts are >= 1
+
+      Directions.STOP:
+        The stop direction, which is always legal
+
+      gameState.generateSuccessor(agentIndex, action):
+        Returns the successor game state after an agent takes an action
+
+      gameState.getNumAgents():
+        Returns the total number of agents in the game
+	
+      gameState.isWin():
+        Returns True if it's a winning state
+	
+      gameState.isLose():
+        Returns True if it's a losing state
+
+      self.depth:
+        The depth to which search should continue
+    """
+
+    # BEGIN_YOUR_CODE (around 68 lines of code expected)
+    score,action = self.getBestMinimaxValue(gameState,0,0)
+    return action
+    # END_YOUR_CODE
+
+class AlphaBetaAgent(MultiAgentSearchAgent):
+  """
+    Your minimax agent with alpha-beta pruning (problem 2)
+  """
+  def getBestAlphaBetaValue(self,gameState,currDepth,agentNumber,alpha,beta):
+    newDepth = currDepth
+    newAgentNumber = agentNumber
+    if agentNumber == gameState.getNumAgents():
+      newDepth+=1
+      newAgentNumber=0
+    if gameState.isWin() or gameState.isLose() or newDepth == self.depth: #depth start at 0
+      return (self.evaluationFunction(gameState),Directions.STOP)
+
+    elif newAgentNumber == 0:
+      return self.getMaxValue(gameState,newDepth,newAgentNumber,alpha,beta) #just agent Number
+
+    elif newAgentNumber >= 1:
+      return self.getMinValue(gameState,newDepth,newAgentNumber,alpha,beta)
+    else:
+      assert False
+
+  def getMaxValue(self,gameState,currDepth,agentNumber,alpha,beta):
+    maxValue = -sys.maxint
+    bestAction = 0
+    assert agentNumber < gameState.getNumAgents()
+    assert agentNumber == 0
+    for action in gameState.getLegalActions(agentNumber):
+      if action == Directions.STOP:
+        continue
+      newGameState = gameState.generateSuccessor(agentNumber,action)
+      currMax = self.getBestAlphaBetaValue(newGameState,currDepth,agentNumber+1,alpha,beta)[0]
+      if currMax >= beta:
+        return (currMax,action)
+      alpha = max(alpha,currMax)
+      if currMax > maxValue:
+        maxValue = currMax
+        bestAction = action
+    return (maxValue,bestAction)
+
+  def getMinValue(self,gameState,currDepth,agentNumber,alpha,beta):
+    minValue = sys.maxint
+    bestAction = 0
+    assert agentNumber < gameState.getNumAgents()
+    assert agentNumber != 0
+    for action in gameState.getLegalActions(agentNumber):
+      if action == Directions.STOP:
+        continue
+      newGameState = gameState.generateSuccessor(agentNumber,action)
+      currMin = self.getBestAlphaBetaValue(newGameState,currDepth,agentNumber+1,alpha,beta)[0]
+      if currMin <=alpha:
+        return (currMin,action)
+      beta = min(currMin,beta)
+      if currMin < minValue:
+        minValue = currMin
+        bestAction = action
+    return (minValue,bestAction)
+  def getAction(self, gameState):
+    """
+      Returns the minimax action using self.depth and self.evaluationFunction
+    """
+
+    # BEGIN_YOUR_CODE (around 69 lines of code expected)
+    score,action = self.getBestAlphaBetaValue(gameState,0,0,-float('inf'),float('inf'))
+    return action
+    # END_YOUR_CODE
+
+class ExpectimaxAgent(MultiAgentSearchAgent):
+  """
+    Your expectimax agent (problem 3)
+  """
+  def getBestExpectimaxValue(self,gameState,currDepth,agentNumber):
+    newDepth = currDepth
+    newAgentNumber = agentNumber
+    if agentNumber == gameState.getNumAgents():
+      newDepth+=1
+      newAgentNumber=0
+    if gameState.isWin() or gameState.isLose() or newDepth == self.depth: #depth start at 0
+      return (self.evaluationFunction(gameState),Directions.STOP)
+
+    elif newAgentNumber == 0:
+      return self.getMaxValue(gameState,newDepth,newAgentNumber) #just agent Number
+
+    elif newAgentNumber >= 1:
+      return self.getMinValue(gameState,newDepth,newAgentNumber)
+    else:
+      assert False
+
+  def getMaxValue(self,gameState,currDepth,agentNumber):
+    maxValue = -sys.maxint
+    bestAction = 0
+    assert agentNumber < gameState.getNumAgents()
+    assert agentNumber == 0
+    for action in gameState.getLegalActions(agentNumber):
+      if action == Directions.STOP:
+        continue
+      newGameState = gameState.generateSuccessor(agentNumber,action)
+      currMax = self.getBestExpectimaxValue(newGameState,currDepth,agentNumber+1)[0]
+      if currMax > maxValue:
+        maxValue = currMax
+        bestAction = action
+    return (maxValue,bestAction)
+
+  def getMinValue(self,gameState,currDepth,agentNumber):
+    minValue = sys.maxint
+    bestAction = 0
+    assert agentNumber < gameState.getNumAgents()
+    assert agentNumber != 0
+    totalValue = 0.0
+    totalActions = len(gameState.getLegalActions(agentNumber))
+    if Directions.STOP in gameState.getLegalActions(agentNumber):
+      totalActions-=1
+    for action in gameState.getLegalActions(agentNumber):
+      if action == Directions.STOP:
+        continue
+      newGameState = gameState.generateSuccessor(agentNumber,action)
+      currMin = self.getBestExpectimaxValue(newGameState,currDepth,agentNumber+1)[0]
+      totalValue+=currMin
+    average = totalValue/totalActions
+    return (average,'None')
+  def getAction(self, gameState):
+    """
+      Returns the expectimax action using self.depth and self.evaluationFunction
+
+      All ghosts should be modeled as choosing uniformly at random from their
+      legal moves.
+    """
+
+    # BEGIN_YOUR_CODE (around 70 lines of code expected)
+    score,action = self.getBestExpectimaxValue(gameState,0,0)
+    return action
+    # END_YOUR_CODE
+
+def betterEvaluationFunction(currentGameState):
+  """
+    Your extreme ghost-hunting, pellet-nabbing, food-gobbling, unstoppable
+    evaluation function (problem 4).
+
+    DESCRIPTION: I used the distance to the closest ghost (1 over that value) and the distance to the closest food and the original getScore function
+    I weighted more heavily the ghost because dying is the worst possible thing, but to avoid it from trashing in the corner I also had to find a good weight
+    for the food so it will actually have incentive to go to it. The original getScore was just a good index to start from.
+  """
+
+  # BEGIN_YOUR_CODE (around 69 lines of code expected)
+  #currEvaluationFunction = self.evaluationFunction(currentGameState)
+  distancesToGhosts = 0
+  pacPos = currentGameState.getPacmanPosition()
+  foodPos = currentGameState.getFood()
+  distanceToClosestGhost=float('inf')
+  for ghostIndex in range(1,currentGameState.getNumAgents()):
+    distance = manhattanDistance(pacPos,currentGameState.getGhostPosition(ghostIndex))
+    if distance < distanceToClosestGhost:
+      distanceToClosestGhost = distance
+    distancesToGhosts+=distance
+
+  totalFoodDistances=0
+  closestFood = float('inf')
+  for x in range(foodPos.width):
+    for y in range(foodPos.height):
+      if foodPos[x][y] == True:
+        distance = manhattanDistance((x,y),pacPos)
+        totalFoodDistances+= distance
+        if closestFood>distance:
+          closestFood = distance
+  #currEvalFnWeight = 1
+  ghostWeight = 1000
+  foodWeight = 1
+  scoreWeight = 100
+  if distanceToClosestGhost == 0:
+    return -10000
+  score =  -ghostWeight*1/distanceToClosestGhost + foodWeight*closestFood + scoreWeight * currentGameState.getScore()
+  return  score
+
+  # END_YOUR_CODE
+
+# Abbreviation
+better = betterEvaluationFunction
+
+
